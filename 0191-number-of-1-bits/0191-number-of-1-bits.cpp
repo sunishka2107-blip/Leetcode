aed94e058a83a1f1cc30729a87c04f2c75ac296a
Sunishka Agarwal <sunishka2107@gmail.com>
2026-08-23 22:03:56 +0530

    Time: 0 ms (100%), Space: 8.1 MB (82.7%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int count = 0 ; 
+        while (n>0){
+            int temp = n%2 ; 
+            if (temp==1){
+                count++ ;
+            }
+            n=n/2 ; 
+        }
+        return count ; 
+        
+    }
+};
