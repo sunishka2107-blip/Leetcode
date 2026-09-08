5771f13b7bb5aec0d15ef4e9577a73a213c16277
Sunishka Agarwal <sunishka2107@gmail.com>
2026-09-08 20:20:39 +0530

    Time: 0 ms (100%), Space: 8.6 MB (15.51%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        int count = 0 ; 
+        if(n<1000){
+            return 0; 
+        }
+        return n-999 ; 
+
+
+        
+    }
+};
